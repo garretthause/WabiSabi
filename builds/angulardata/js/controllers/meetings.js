ce4e8ea265c60d87952d838df67950584cf5c1b5
John Fowler <krasez@yahoo.com>
2016-04-26 12:09:33 -0600

    fixed delete error

--- builds/angulardata/js/controllers/meetings.js
+++ builds/angulardata/js/controllers/meetings.js
@@ -15,11 +15,6 @@ $scope.orderByDate = function(item) {
        onSelect: function(dateText, inst) { 
           $scope.classDate = dateText;
           this.focus();
-          var parts = dateText.split('/');
-    var newDate = parts[2]+
-                        parts[1]+ 
-                        parts[0];
-                        alert(newDate);
         }
     });
 
