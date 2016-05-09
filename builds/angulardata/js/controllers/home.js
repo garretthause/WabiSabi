21e28046b776eb9d88dc1168480ceb1bd7468298
garretthause <garretthause@gmail.com>
2016-05-09 02:44:23 -0600

    fix errors

--- builds/angulardata/js/controllers/home.js
+++ builds/angulardata/js/controllers/home.js
@@ -1,6 +1,6 @@
 
-artistControllers.controller('HomeController', ['$scope', '$http', function ($scope, $http){
-        $http.get('js/data.json').success(function(data) {
+artistControllers.controller('HomeController', ['$scope', '$https', function ($scope, $https){
+        $https.get('js/data.json').success(function(data) {
           $scope.artists = data;
           $scope.artistOrder = 'name';
         });
